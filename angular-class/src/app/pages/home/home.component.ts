fa65ff670514776da6dbe6b280d9b69375240574
Raphael_Notbook <raphael.braga.2001@gmail.com>
2025-07-17 10:04:45 -0300

    Correção no codigo do Carrossel

--- angular-class/src/app/pages/home/home.component.ts
+++ angular-class/src/app/pages/home/home.component.ts
@@ -1,12 +1,11 @@
 import { Component, inject } from '@angular/core';
-import { BoasVindasComponent } from "../../components/boas-vindas/boas-vindas.component";
 import { MenuComponent } from "../../components/menu/menu.component";
 import { Router } from '@angular/router';
 import { CarouselComponent } from "../../components/carousel/carousel.component";
 
 @Component({
   selector: 'app-home',
-  imports: [BoasVindasComponent, MenuComponent, CarouselComponent],
+  imports: [MenuComponent, CarouselComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
